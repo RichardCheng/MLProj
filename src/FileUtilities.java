ac2560ab5d6719674f7f4912fb18587e4ac877e5
U-RainG-PC\RainG <rain.gc@gmail.com>
2011-09-18 17:54:31 -0400

    Changed Fileutilities.

--- src/FileUtilities.java
+++ src/FileUtilities.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  * @author hank.duan
  * 
  */
-public class Utilities {
+public class FileUtilities {
 
 	/**
 	 * write (String data, String fileloc) writes data to a file with its
@@ -91,47 +91,5 @@ public class Utilities {
 		
 		return contents;
 	}
-	
-	//find euclidean distance between two entries
-	public static double euc_dist (Entry e1, Entry e2) throws Exception{
-		Entry.check(e1, e2); 
-		double sum = 0; 
-		for (int i = 0; i < e1.features.length; i++){
-			sum += (Math.pow(e1.features[i] - e2.features[i], 2.0)); 
-		}
-	
-		return Math.sqrt(sum); 
-	}
-	
-	public static double get_average (ArrayList<Double> lst){
-		double sum = 0; 
-		for (Double d : lst){
-			sum += d;
-		}
-		return sum/lst.size(); 
-		
-	}
-	
-	public static double get_stddev (ArrayList<Double> lst){
-		double sum = 0;
-		double average = get_average(lst); 
-		for (Double d : lst){
-			sum += (d - average) * (d-average);
-		}
-		return Math.sqrt(sum/lst.size()); 
-	}
-	
-	// Compares two doubles to see if they're equal
-	boolean DoubleEquals (double A, double B){
-	    if (A == B)
-	        return true;
-	    
-		double maxRelativeError = 0.0001; 
-	    double relativeError;
-	    if (B > A)
-	        relativeError = (B - A) / B;
-	    else
-	        relativeError = (A - B) / A;
-	    return relativeError <= maxRelativeError;
-	}
+
 }
